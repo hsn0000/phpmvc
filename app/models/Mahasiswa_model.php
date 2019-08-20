38e046fff1382b977be36d4684b31decc9922c89
Husin <Muhamad.husin0412@gmail.com>
2019-08-20 23:08:07 +0700

    crud

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -31,16 +31,40 @@ class Mahasiswa_model {
           $this->db = new Database;
       }
 
-   public function getAllMahasiswa() {
+    public function getAllMahasiswa() {
        $this->db->query('SELECT * FROM ' . $this->table);
        return $this->db->resultSet();
 
- }
- public function getMahasiswabyId($id) {
-     $this->db->query('SELECT * FROM ' . $this->table . 'WHERE id=:id');
-     $this->db->bind('id', $id);
-     return $this->db->single();
- }
+    }
+    public function getMahasiswaById($id) {
+        $this->db->query('SELECT * FROM ' . $this->table . 'WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
+    public function tambahDataMahasiswa($data) {
+        $query = "INSERT INTO mahasiswa VALUES ('', :nama, :nrp, :email, :jurusan)";
+
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+        
+        return $this->db->rowCount();
+    }
+
+    public function hapusDataMahasiswa($id) {
+        $query = "DELETE FROM mahasiswa WHERE id = :id";
+        $query->db->query($query);
+        $query->db->bind('id', $id);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
 
 }
 
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,6 @@
 <div class="container">
   <h1 class="mt-4">About me</h1>
-  <img src="<?= BASEURL; ?>/img/profile_naruto.jpg" alt="Profile Ku" width="230" class="rounded-circle shadow">
-  <p>Hallo, nama saya <?= $data['nama']; ?> saya seorang <?= $data['pekerjaan']; ?></p>
+  <img src="<?= BASEURL; ?>/img/profile_naruto.jpg" alt="Profile Ku" width="230" class="rounded-circle shadow mt-2">
+  <br>
+  <p class="mt-3">Hallo, nama saya <?= $data['nama']; ?> saya seorang <?= $data['pekerjaan']; ?></p>
 </div>
