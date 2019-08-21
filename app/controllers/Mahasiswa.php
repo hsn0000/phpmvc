90b60c90d271a9cc1d8a111695883ff5dace1f6f
MHusin <Muhamad.husin120495@gmail.com>
2019-08-21 15:57:00 +0700

    update data (ajax)

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -28,16 +28,36 @@ class Mahasiswa  extends Controller {
 
         }
     }
-    // public function hapus($id) {
-    //     if( $this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0 ) {
-    //         Flasher::setFlash('berhasil', 'dihapus', 'success');
-    //         header('Location:' . BASEURL . '/mahasiswa');
-    //         exit;
-    //     }else{
-    //         Flasher::setFlash('gagal', 'dihapus', 'danger');
-    //         header('Location:' . BASEURL . '/mahasiswa');
-    //         exit;
+    public function hapus($id) {
+        if( $this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0 ) {
+            Flasher::setFlash('berhasil', 'dihapus', 'success');
+            header('Location:' . BASEURL . '/mahasiswa');
+            exit;
+        }else{
+            Flasher::setFlash('gagal', 'dihapus', 'danger');
+            header('Location:' . BASEURL . '/mahasiswa');
+            exit;
 
-    //     }
-    // }
+        }
+    }
+
+    public function getubah() {
+     json_encode ( $this->model('Mahasiswa_model')->getMahasiswaById($_POST['id']));
+
+    }
+
+    public function ubah() {
+       
+        if( $this->Model('Mahasiswa_model')->ubahDataMahasiswa($_POST) > 0 ) {
+            Flasher::setFlash('berhasil', 'diubah', 'success');
+            header('Location:' . BASEURL . '/mahasiswa');
+            exit;
+        }else{
+            Flasher::setFlash('gagal', 'diubah', 'danger');
+            header('Location:' . BASEURL . '/mahasiswa');
+            exit;
+
+        }
+    }
+
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -8,7 +8,7 @@
 
     <div class="row">
        <div class="col-lg-6">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+        <button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
          Tambah Data Mahasiswa
          </button>
          <br><br>
@@ -19,6 +19,9 @@
                  <?= $mhs['nama']; ?>
                  <a href=" <?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?> "
                   class="badge badge-danger float-right ml-1" onclick=" return confirm('YAKIN INGIN MENGHAPUS..!');">Hapus</a>
+                  <a href=" <?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id']; ?> " 
+                   class="badge badge-success float-right ml-2 tampilModalUbah" data-toggle="modal" data-target="#formModal"
+                   data-id= "<?= $mhs['id']; ?> " >Ubah</a>
                   <a href=" <?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?> " 
                   class="badge badge-warning float-right ml-2">Detail</a>
                 </li>
@@ -29,11 +32,11 @@
    
 </div>
 
-<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="formModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -41,6 +44,7 @@
       <div class="modal-body">
         
     <form action=" <?= BASEURL; ?>/mahasiswa/tambah " method="post">
+      <input type="hidden" name="id" id="id">
       <div class="form-group">
        <label for="nama">Nama</label>
         <input type="text" class="form-control" id="nama" name="nama">
